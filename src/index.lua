1c14340378dd7b8d4dffce1637f6b2e03a697e41
gnmmarechal <gnmpolicemata91@gmail.com>
2017-08-05 23:28:58 +0100

    Updated to reflect usage of R4

--- src/index.lua
+++ src/index.lua
@@ -8,8 +8,11 @@ local version = "1.0.0"
 local backupDir = "ux0:/data/vita-backup-utility"
 
 local white = Color.new(255, 255, 255, 255)
-
+local oldpad = Controls.read()
 -- Main Methods
+
+System.createDirectory("ux0:/data")
+System.createDirectory(backupDir)
 
 function getBackupDir()
 	h, m, s = System.getTime()
@@ -17,37 +20,36 @@ function getBackupDir()
 	return backupDir.."/backup-"..y.."-"..m.."-".."d"
 end
 
-function copyFile(fileName, newFileName)
-	if System.doesFileExist(fileName) then
-		local fileStream = io.open(fileName, FCREATE)
-		local content = io.read(fileStream, io.size(fileStream))
-		local newFileStream = io.open(newFileName, FCREATE)
-		io.write(newFileStream, content, io.size(fileStream))
-		io.close(fileStream)
-		io.close(newFileStream)
-		return 0
+function copyFile(input, output)
+	inp = io.open(input, FREAD)
+	if System.doesFileExist(output) then
+		System.deleteFile(output)
 	end
-	return 1
+	out = io.open(output,FCREATE)
+	size = io.size(inp)
+	io.write(out, io.read(inp, size), size)
+	io.close(inp)
+	io.close(out)
 end
 
 function doScene(number)
-	if number == 1 then
+	if number == 3 then
 		Graphics.debugPrint(5, 5, "Vita Backup Utility v"..version, white)
 		Graphics.debugPrint(5, 45, "X - Backup Files", white)
 		if Controls.check(Controls.read(), SCE_CTRL_START) and not Controls.check(oldpad, SCE_CTRL_START) then
-			scene = 2
-			return
-		end
-		if Controls.check(Controls.read(), SCE_CTRL_START) and not Controls.check(oldpad, SCE_CTRL_START) then
 			System.exit()
 		end
+		scene = 2
+		return
+
 	elseif number == 2 then
 		Graphics.debugPrint(5, 5, "Backing up files...", white)
 		scene = 3
 		return
-	elseif number == 3 then
+	elseif number == 1 then
 		Graphics.debugPrint(5, 5, "Backing up files...", white)
-		local backupPath = getBackupDir()
+		--local backupPath = getBackupDir()
+		local backupPath = "ux0:/data/vita-backup-utility"
 		-- Activation Files
 		copyFile("tm0:/npdrm/act.dat", backupPath.."/tm0_npdrm_act.dat")
 		copyFile("vd0:/registry/system.dreg", backupPath.."/vd0_registry_system.dreg")
@@ -55,14 +57,14 @@ function doScene(number)
 		-- Required for Remote Play
 		copyFile("ur0:/user/00/np/myprofile.dat", backupPath.."/ur0_user_00_np_myprofile.dat")
 		scene = 4
-		return
+		System.exit()
 	elseif number == 4 then
 		Graphics.debugPrint(5, 5, "Finished Backup! Press X to return to the main menu")
 		scene = 5		
 		return
 	elseif number == 5 then
 		Graphics.debugPrint(5, 5, "Finished Backup! Press X to return to the main menu")
-		Controls.check(Controls.read(), SCE_CTRL_CROSS) and not Controls.check(oldpad, SCE_CTRL_CROSS) then
+		if Controls.check(Controls.read(), SCE_CTRL_CROSS) and not Controls.check(oldpad, SCE_CTRL_CROSS) then
 			scene = 1
 			return		
 		end
@@ -71,7 +73,6 @@ end
 -- Main Loop
 local scene = 1
 local running = true
-local oldpad = Controls.read()
 while running do
 	Graphics.initBlend()
 	Screen.clear()
